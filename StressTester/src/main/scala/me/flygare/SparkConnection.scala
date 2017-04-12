5ed27b18963809bdaa5f0b62cd562a10b7962b66
Robin Flygare <robin.flygare1@gmail.com>
2017-04-12 16:59:51 +0200

    Add spark connection trait

---
+++ StressTester/src/main/scala/me/flygare/SparkConnection.scala
@@ -0,0 +1,16 @@
+package me.flygare
+
+import com.datastax.spark.connector.cql.CassandraConnector
+import org.apache.spark.{SparkConf, SparkContext}
+
+trait SparkConnection {
+  val conf = new SparkConf(true)
+    .set("spark.cassandra.connection.host", "127.0.0.1")
+    .set("spark.cassandra.auth.username", "cassandra")
+    .set("spark.cassandra.auth.password", "cassandra")
+
+  val sc = new SparkContext("spark://127.0.0.1:7077", "test", conf)
+
+  val connector = CassandraConnector(sc.getConf)
+  var session = connector.openSession()
+}
