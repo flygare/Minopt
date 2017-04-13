0b783c921e8492ceea7b56cdd1c5827999fb0015
Robin Flygare <robin.flygare1@gmail.com>
2017-04-13 11:31:43 +0200

    basic app with spark

--- StressTester/src/main/scala/me/flygare/SparkConnection.scala
+++ StressTester/src/main/scala/me/flygare/SparkConnection.scala
@@ -5,6 +5,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 trait SparkConnection {
   val conf = new SparkConf(true)
+    .setMaster("local[*]")
     .set("spark.cassandra.connection.host", "127.0.0.1")
     .set("spark.cassandra.auth.username", "cassandra")
     .set("spark.cassandra.auth.password", "cassandra")
@@ -13,4 +14,6 @@ trait SparkConnection {
 
   val connector = CassandraConnector(sc.getConf)
   var session = connector.openSession()
+  session.execute("CREATE KEYSPACE IF NOT EXISTS test WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1 };")
+  session.execute("CREATE TABLE IF NOT EXISTS test.kv(id INT PRIMARY KEY, ts timestamp);")
 }
--- StressTester/src/main/scala/me/flygare/StressTester.scala
+++ StressTester/src/main/scala/me/flygare/StressTester.scala
@@ -1,5 +1,8 @@
 package me.flygare
 
-object StressTester extends App{
+object StressTester extends App with SparkConnection{
   print("Hello")
+  session.execute("INSERT INTO test.kv ")
+  val rdd = sc.cassandraTable("test", "kv")
+  println(rdd.count)
 }
