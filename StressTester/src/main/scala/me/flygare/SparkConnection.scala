00a5fb162d732575b579171a3adb2ab27cd06be0
Anthon Holmqvist <anthon.kendel@gmail.com>
2017-04-13 16:32:05 +0200

    Add basic functionality with datasets

--- StressTester/src/main/scala/me/flygare/SparkConnection.scala
+++ StressTester/src/main/scala/me/flygare/SparkConnection.scala
@@ -3,8 +3,10 @@ package me.flygare
 import com.datastax.driver.core.Session
 import com.datastax.spark.connector.cql.CassandraConnector
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
 
 trait SparkConnection {
+  val warehouseLocation = "file:${system:user.dir}/spark-warehouse"
   val conf: SparkConf = new SparkConf(true)
     .setAppName("Minopt")
     .setMaster("local[2]")
@@ -12,13 +14,19 @@ trait SparkConnection {
     .set("spark.cassandra.auth.username", "cassandra")
     .set("spark.cassandra.auth.password", "cassandra")
 
-  val sc: SparkContext = new SparkContext(conf)
+  val spark: SparkSession = SparkSession
+    .builder()
+    .appName("Minopt")
+    .config(conf)
+    .getOrCreate()
 
-  val connector: CassandraConnector = CassandraConnector(sc.getConf)
+  val connector: CassandraConnector = CassandraConnector(spark.sparkContext)
   var session: Session = connector.openSession()
 
   val keyspace = "minopt"
 
   session.execute("CREATE KEYSPACE IF NOT EXISTS " + keyspace + " WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1 };")
   session.execute("CREATE TABLE IF NOT EXISTS " + keyspace + ".kv(id INT PRIMARY KEY, ts timestamp);")
+
+  session.close()
 }
--- StressTester/src/main/scala/me/flygare/StressTester.scala
+++ StressTester/src/main/scala/me/flygare/StressTester.scala
@@ -1,11 +1,40 @@
 package me.flygare
+import org.apache.spark.sql.cassandra._
 import com.datastax.spark.connector._
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.execution.streaming.FileStreamSource.Timestamp
+import org.joda.time.{DateTime, DateTimeZone}
+import scala.util.Random
 
-import com.datastax.spark.connector._
+
+case class KeyValue(id: Int, ts: String)
 
 object StressTester extends App with SparkConnection{
-  println("Hello")
+  val datasetFormat = "org.apache.spark.sql.cassandra"
 
-  val rdd = sc.cassandraTable("minopt", "kv")
-  println("Cassanda rows: " + rdd.count)
+  import spark.implicits._
+
+  var keyValue: Seq[KeyValue] = Seq.empty
+  var rows: Int = 0
+  while (rows < 10000) {
+    keyValue ++= Seq(KeyValue(Random.nextInt(1000), DateTime.now(DateTimeZone.UTC).toString()))
+    rows += 1
+  }
+
+
+
+
+  keyValue.toDS()
+    .write
+    .format(datasetFormat)
+    .options(Map( "table" -> "kv", "keyspace" -> "minopt"))
+    .mode("overwrite")
+    .cassandraFormat("keyValue", "minopt")
+    .save()
+
+  val df = spark
+    .read
+    .format("org.apache.spark.sql.cassandra")
+    .options(Map( "table" -> "keyValue", "keyspace" -> "minopt"))
+    .load().show()
 }
