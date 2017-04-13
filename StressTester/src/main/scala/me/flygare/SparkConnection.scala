c6997a67c399e5b4d9866c3c74699dffb2b3d0fb
Robin Flygare <robin.flygare1@gmail.com>
2017-04-13 11:43:49 +0200

    Fix basic spark connection

--- StressTester/src/main/scala/me/flygare/SparkConnection.scala
+++ StressTester/src/main/scala/me/flygare/SparkConnection.scala
@@ -14,6 +14,6 @@ trait SparkConnection {
 
   val connector = CassandraConnector(sc.getConf)
   var session = connector.openSession()
-  session.execute("CREATE KEYSPACE IF NOT EXISTS test WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1 };")
+  session.execute("CREATE KEYSPACE test WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1 };")
   session.execute("CREATE TABLE IF NOT EXISTS test.kv(id INT PRIMARY KEY, ts timestamp);")
 }
