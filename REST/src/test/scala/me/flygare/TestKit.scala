4d93d5b1f605c30fd6325a8e4eedac8c29fba217
Robin Flygare <robin.flygare1@gmail.com>
2017-04-19 22:10:15 +0200

    Add config to test

--- REST/src/test/scala/me/flygare/TestKit.scala
+++ REST/src/test/scala/me/flygare/TestKit.scala
@@ -1,11 +1,12 @@
 package me.flygare
 
-import org.scalatest.{ Matchers, WordSpec }
+import org.scalatest.{Matchers, WordSpec}
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.http.scaladsl.server._
+import me.flygare.utils.HttpConfig
 
-class TestKit extends WordSpec with Matchers with ScalatestRouteTest {
+class TestKit extends WordSpec with Matchers with ScalatestRouteTest with HttpConfig{
 
   "The service" should {
 
