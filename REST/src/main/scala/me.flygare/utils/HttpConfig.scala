bd9fb7da5157b30be5776e5edcbed7e5426d8e93
Robin Flygare <robin.flygare1@gmail.com>
2017-04-19 22:21:24 +0200

    Test fix test on travis

--- REST/src/main/scala/me.flygare/utils/HttpConfig.scala
+++ REST/src/main/scala/me.flygare/utils/HttpConfig.scala
@@ -1,9 +1,13 @@
 package me.flygare.utils
 
+import java.io.File
+
 import com.typesafe.config.ConfigFactory
 
 trait HttpConfig{
-  val port = ConfigFactory.load.getInt("http.local.port")
-  val interface = ConfigFactory.load.getString("http.local.interface")
-  val remoteHost = ConfigFactory.load.getString("http.remote.interface")
+  val parsedConfig = ConfigFactory.parseFile(new File("src/main/resources/application.conf"))
+
+  val port = parsedConfig.getInt("http.local.port")
+  val interface = parsedConfig.getString("http.local.interface")
+  val remoteHost = parsedConfig.getString("http.remote.interface")
 }
