e6ec3db66293376c13410d9602878d0b8c835e37
Anthon Holmqvist <anthon.kendel@gmail.com>
2017-04-19 15:33:41 +0200

    Add code for send POST request

--- Website/js/main.js
+++ Website/js/main.js
@@ -3,17 +3,51 @@ const BASEURL = "http://0.0.0.0:3000";
 $(function(){
     $("#get-btn").click(function(){
         console.log("GET");
-        $.get("http://mean-quote-machine.herokuapp.com/get_random_quote", function(data) {
-            var json = JSON.parse(JSON.stringify(data));
-            var author = json.quote.author;
-            var text = json.quote.text;
+        $.ajax({
+            type: "GET",
+            url: "http://mean-quote-machine.herokuapp.com/get_random_quote",
+            contentType: "application/json; charset=utf-8",
+            dataType: "jsonp",
+            success: function(data) {
+                var json = JSON.parse(JSON.stringify(data));
+                var author = json.quote.author;
+                var text = json.quote.text;
 
-            $("#quote-author").text(author);
-            $("#quote-text").text(text);
+                $("#quote-author").text(author);
+                $("#quote-text").text(text);
+            },
+            error: function(error) {
+                console.error(error);
+            }
         });
     });
 
     $("#post-btn").click(function(){
         console.log("POST");
+        var tmp = JSON.stringify({
+            "author": "Robin Flygare and Anthon Holmqvist",
+            "text": "Minopt"
+        });
+
+        console.log(tmp);
+
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:1880/",
+            contentType: "application/json; charset=utf-8",
+            dataType: "jsonp",
+            data: tmp,
+            success: function(data) {
+                var json = JSON.parse(JSON.stringify(data));
+                var author = json.quote.author;
+                var text = json.quote.text;
+
+                $("#quote-author").text(author);
+                $("#quote-text").text(text);
+            },
+            error: function(error) {
+                console.error(error);
+            }
+        });
     });
 });
