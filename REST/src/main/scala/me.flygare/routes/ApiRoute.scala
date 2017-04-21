e284a53b92d7984bd913f71489c749b210b2cc63
Anthon Holmqvist <anthon.kendel@gmail.com>
2017-04-21 10:59:40 +0200

    Add cross-origin headers

--- REST/src/main/scala/me.flygare/routes/ApiRoute.scala
+++ REST/src/main/scala/me.flygare/routes/ApiRoute.scala
@@ -5,51 +5,54 @@ import akka.http.scaladsl.model.HttpRequest
 import akka.http.scaladsl.server.Directives._
 import me.flygare.utils.{HttpConfig, HttpConnection}
 import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model.headers.RawHeader
 
-object ApiRoute extends HttpConnection with HttpConfig{
- val route =
-  pathPrefix("api") {
-    delete {
-      complete(Http().singleRequest(HttpRequest(DELETE, uri = s"$remoteHost:$remotePort/dblogic")))
-    }
-    path("persons"){
-      get {
-        parameters('rows) {
-          (rows) =>
-            complete(Http().singleRequest(HttpRequest(uri = s"$remoteHost:$remotePort/dblogic/persons?rows=$rows")))
+object ApiRoute extends HttpConnection with HttpConfig {
+  val route =
+    pathPrefix("api") {
+      respondWithDefaultHeaders(RawHeader("Access-Control-Allow-Origin", "*"), RawHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")) {
+        delete {
+          complete(Http().singleRequest(HttpRequest(DELETE, uri = s"$remoteHost:$remotePort/dblogic")))
         }
-      }~
-      post {
-        entity(as[String]){
-          data => complete(Http().singleRequest(HttpRequest(POST, uri = s"$remoteHost:$remotePort/dblogic/persons", entity = data)))
-        }
+        path("persons") {
+          get {
+            parameters('rows) {
+              (rows) =>
+                complete(Http().singleRequest(HttpRequest(uri = s"$remoteHost:$remotePort/dblogic/persons?rows=$rows")))
+            }
+          } ~
+            post {
+              entity(as[String]) {
+                data => complete(Http().singleRequest(HttpRequest(POST, uri = s"$remoteHost:$remotePort/dblogic/persons", entity = data)))
+              }
+            }
+        } ~
+          path("addresses") {
+            get {
+              parameters('rows) {
+                (rows) =>
+                  complete(Http().singleRequest(HttpRequest(uri = s"$remoteHost:$remotePort/dblogic/addresses?rows=$rows")))
+              }
+            } ~
+              post {
+                entity(as[String]) {
+                  data => complete(Http().singleRequest(HttpRequest(POST, uri = s"$remoteHost:$remotePort/dblogic/addresses", entity = data)))
+                }
+              }
+          } ~
+          path("profiles") {
+            get {
+              parameters('rows) {
+                (rows) =>
+                  complete(Http().singleRequest(HttpRequest(uri = s"$remoteHost:$remotePort/dblogic/profiles?rows=$rows")))
+              }
+            } ~
+              post {
+                entity(as[String]) {
+                  data => complete(Http().singleRequest(HttpRequest(POST, uri = s"$remoteHost:$remotePort/dblogic/profiles", entity = data)))
+                }
+              }
+          }
       }
-    }~
-    path("addresses"){
-      get {
-        parameters('rows) {
-          (rows) =>
-            complete(Http().singleRequest(HttpRequest(uri = s"$remoteHost:$remotePort/dblogic/addresses?rows=$rows")))
-        }
-      }~
-        post {
-          entity(as[String]){
-            data => complete(Http().singleRequest(HttpRequest(POST, uri = s"$remoteHost:$remotePort/dblogic/addresses", entity = data)))
-          }
-        }
-    }~
-    path("profiles"){
-      get {
-        parameters('rows) {
-          (rows) =>
-            complete(Http().singleRequest(HttpRequest(uri = s"$remoteHost:$remotePort/dblogic/profiles?rows=$rows")))
-        }
-      }~
-        post {
-          entity(as[String]){
-            data => complete(Http().singleRequest(HttpRequest(POST, uri = s"$remoteHost:$remotePort/dblogic/profiles", entity = data)))
-          }
-        }
     }
-  }
 }
--- REST/src/main/scala/me.flygare/routes/TestRoute.scala
+++ REST/src/main/scala/me.flygare/routes/TestRoute.scala
@@ -2,18 +2,21 @@ package me.flygare.routes
 
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.server.Directives.{complete, get, path}
 import akka.http.scaladsl.server.Directives._
 import me.flygare.utils.HttpConnection
 
-object TestRoute extends HttpConnection{
+object TestRoute extends HttpConnection {
   val route =
     pathPrefix("test") {
-      path("ok"){
-        complete("Ok")
-      }~
-      path("json"){
-        complete(Http().singleRequest(HttpRequest(uri = s"http://jsonplaceholder.typicode.com/posts/1")))
+      respondWithDefaultHeaders(RawHeader("Access-Control-Allow-Origin", "*"), RawHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")) {
+        path("ok") {
+          complete("Ok")
+        } ~
+          path("json") {
+            complete(Http().singleRequest(HttpRequest(uri = s"http://jsonplaceholder.typicode.com/posts/1")))
+          }
       }
     }
 }
