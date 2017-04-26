884d4952fa9d39d4e214bd863cfd1b191e6f6a8a
Robin Flygare <robin.flygare1@gmail.com>
2017-04-26 14:46:56 +0200

    Fix config bug

--- RestService/src/main/scala/me.flygare/utils/HttpConfig.scala
+++ RestService/src/main/scala/me.flygare/utils/HttpConfig.scala
@@ -1,18 +1,13 @@
 package me.flygare.utils
 
-import java.io.File
-import com.typesafe.config.ConfigFactory
-
 trait HttpConfig{
-  val parsedConfig = ConfigFactory.parseFile(new File("RestService/src/main/resources/application.conf"))
-  val conf = ConfigFactory.load(parsedConfig)
 
-  val interface = conf.getString("http.local.interface")
-  val port = conf.getInt("http.local.port")
+  val interface = "0.0.0.0"
+  val port = 3000
 
-  val remoteHost = conf.getString("http.remote.interface")
-  val remoteAddressPort = conf.getInt("http.remote.addressPort")
-  val remotePersonPort = conf.getInt("http.remote.personPort")
-  val remoteProfilePort = conf.getInt("http.remote.profilePort")
-  val remotePath = conf.getString("http.remote.path")
+  val remoteHost = "http://localhost"
+  val remoteAddressPort = 3001
+  val remotePersonPort = 3002
+  val remoteProfilePort = 3003
+  val remotePath = "/dblogic"
 }
