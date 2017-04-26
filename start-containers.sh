f678e32515b757ee47c9d5e108bb55290df9bd3f
Robin Flygare <robin.flygare1@gmail.com>
2017-04-26 20:35:41 +0200

    Update containers script with cassandra link

--- start-containers.sh
+++ start-containers.sh
@@ -3,6 +3,9 @@
 docker stop minopt-cassandra 2>/dev/null
 docker rm minopt-cassandra 2>/dev/null
 docker run --name minopt-cassandra -p 9042:9042 -d cassandra:3.0
+docker stop profile-service-cass
+docker rm profile-service-cass
+docker run --name profile-service-cass --link minopt-cassandra:cassandra_profile_service -d profile_service
 
 # docker run --name minopt-person-service
 # docker run --name minopt-address-service
