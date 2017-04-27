4dd511b837b75471927f3da9878274fb514b5a61
Anthon Holmqvist <anthon.kendel@gmail.com>
2017-04-27 09:38:48 +0200

    Add creation of network and network-aliases

--- start-containers.sh
+++ start-containers.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+docker network create minopt 2>/dev/null
+
 docker stop minopt-cassandra 2>/dev/null
 docker rm minopt-cassandra 2>/dev/null
-docker run --name minopt-cassandra -p 9042:9042 -d cassandra:3.0
-docker stop minopt-profile-service
-docker rm minopt-profile-service
-docker run --name minopt-profile-service -d minopt-profile-service
+docker run --network=minopt --network-alias=cassandra --name minopt-cassandra -p 9042:9042 -d cassandra:3.0
+
+sleep 5
+
+docker stop minopt-profile-service 2>/dev/null
+docker rm minopt-profile-service 2>/dev/null
+docker run --network=minopt --network-alias=profile --name minopt-profile-service -d profile-service
 
 # docker run --name minopt-person-service
 # docker run --name minopt-address-service
