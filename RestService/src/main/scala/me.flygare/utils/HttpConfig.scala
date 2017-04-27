3fff98e6648a3d1a1f5be7b9324fd6fd93e38531
Robin Flygare <robin.flygare1@gmail.com>
2017-04-27 11:16:09 +0200

    Fix http:// rest service bug

--- RestService/src/main/scala/me.flygare/utils/HttpConfig.scala
+++ RestService/src/main/scala/me.flygare/utils/HttpConfig.scala
@@ -5,11 +5,11 @@ trait HttpConfig{
   val interface = "0.0.0.0"
   val port = 3000
 
-  val remoteAddressHost = "address"
+  val remoteAddressHost = "http://address"
   val remoteAddressPort = 3001
-  val remotePersonHost = "person"
+  val remotePersonHost = "http://person"
   val remotePersonPort = 3002
-  val remoteProfileHost = "profile"
+  val remoteProfileHost = "http://profile"
   val remoteProfilePort = 3003
   val remotePath = "/dblogic"
 }
