7537b48291cb32af7bdbfba34a5b525e14b265b0
Robin Flygare <robin.flygare1@gmail.com>
2017-05-01 20:15:39 +0200

    Update test and add support for more connections in akka

--- RestService/src/main/scala/me.flygare/RestService.scala
+++ RestService/src/main/scala/me.flygare/RestService.scala
@@ -4,9 +4,9 @@ import akka.http.scaladsl.Http
 import me.flygare.routes.MainRouter
 import me.flygare.utils.{HttpConfig, HttpConnection}
 
-object RestService extends App with HttpConnection with HttpConfig{
+object RestService extends App with HttpConnection with HttpConfig {
 
-  Http().bindAndHandle(MainRouter.routes,s"$interface",port)
+  Http().bindAndHandle(MainRouter.routes, s"$interface", port)
 
   println(s"Server started at $port")
 
