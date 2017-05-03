c61edd6c96d030bd921a6f7c0b406d3f8ecfebc2
Robin Flygare <robin.flygare1@gmail.com>
2017-05-03 18:49:02 +0200

    Redirect output to logfiles

--- start-mono.sh
+++ start-mono.sh
@@ -5,11 +5,11 @@ service cassandra stop
 rm -rf /var/lib/cassandra/data/system/*
 
 # Start cassandra
-service cassandra start
+service cassandra start 2> cassandra.log
 
 sleep 15
 
 # Build and execute Monolithic fat jar
 cd Monolithic/
-nohup java -cp target/scala-2.11/monolithic.jar me.flygare.Monolithic 2>&1 &&
+java -cp target/scala-2.11/monolithic.jar me.flygare.Monolithic 2>monolithic.log
 cd ..
