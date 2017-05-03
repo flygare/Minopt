6f0530a0f2110ff231546dad6d1989020a6e2969
Anthon Holmqvist <anthon.kendel@gmail.com>
2017-05-03 20:55:34 +0200

    Fix expose of port in container

--- start-mono-container.sh
+++ start-mono-container.sh
@@ -5,4 +5,4 @@ MONO_IMAGE=minopt-mono
 
 docker stop $MONO_CONTAINER_NAME 2>/dev/null
 docker rm $MONO_CONTAINER_NAME 2>/dev/null
-docker run --name $MONO_CONTAINER_NAME -d $MONO_IMAGE
+docker run --name $MONO_CONTAINER_NAME -p 3000:3000 -d $MONO_IMAGE
